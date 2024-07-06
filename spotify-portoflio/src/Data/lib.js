af61b9039a5bf0b6ae091b2baa12fad50e931397
AlexVennegas <erick.alexis.venegas@gmail.com>
2024-07-07 01:30:37 +0200

    chore: Refactor article module to dynamically render content based on data

--- spotify-portoflio/src/Data/lib.js
+++ spotify-portoflio/src/Data/lib.js
@@ -38,3 +38,105 @@ export const Information = [
     imageUrl: "./Nasa.jpg",
   }
 ];
+export const Skills = [
+  {
+      id: '1',
+      title: "HTML",
+      description: "lenguaje de marcado que se utiliza para el desarrollo de páginas web",
+  
+      imageUrl: "https://i.scdn.co/image/ab67616d00001e02f0cb4035ee036eb4e42169e5",
+      album: "front-end",
+      time: "2:30",
+  }
+  ,
+  {
+      id: '2',
+      title: "CSS",
+      description: "lenguaje de estilos que se utiliza para el desarrollo de páginas web.",
+      imageUrl: "https://i.scdn.co/image/ab67616d00001e0235692baca45542527d742d0d",
+      album: "front-end",
+      time: "2:30",
+  }
+  ,
+  {
+      id: '3',
+      title: "JavaScript",
+      description: "lenguaje de programación que se utiliza para el desarrollo de aplicaciones web",
+      imageUrl: "https://i.scdn.co/image/ab67616d00001e0276e31f565780a5b45c0bbbd8",
+      album: "back-end",
+      time: "2:30",
+  }
+  ,
+  {
+      id: '4',
+      title: "Python",
+      description: "lenguaje de programación que se utiliza para el desarrollo de aplicaciones web",
+      imageUrl: "https://i2o.scdn.co/image/ab67706c0000cfa320f01f28b8e9f8df357957df",
+      album: "back-end",
+      time: "2:30",
+  
+  },
+  {
+      id: '5',
+      title: "C",
+      description: "lenguaje de programación que se utiliza para el desarrollo de aplicaciones web",
+      imageUrl: "https://i.scdn.co/image/ab67616d00001e02a9249ebb15ca7a5b75f16a90",
+      album: "back-end",
+      time: "2:30",
+  },
+  {
+      id: '6',
+      title: "Bootstrap",
+      description: "framework de CSS.",
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/5968/5968672.png",
+      album: "front-end",
+      time: "2:30",
+  }
+  ,
+  {
+      id: '7',
+      title: "php",
+      description: "lenguaje de programación que se utiliza para el desarrollo de aplicaciones web",
+      imageUrl: "https://i.scdn.co/image/ab67616d00001e02d699829cd4c29b1b669cb201",
+      album: "back-end",
+      time: "2:30",
+  }
+  ,
+  {
+      id: '8',
+      title: "Github",
+      description: "Github es una plataforma de desarrollo colaborativo de software para alojar proyectos utilizando el sistema de control de versiones Git. Se utiliza para el desarrollo de aplicaciones web. Es fundamental para el desarrollo de aplicaciones web escalables y eficientes.",
+      imageUrl: "https://image-cdn-ak.spotifycdn.com/image/ab67706c0000da846c3048465107aa5f0aa17393",
+      album: "back-end",
+      time: "2:30",
+  }
+  ,
+  {
+      id: '9',
+      title: "SQL",
+      description: "SQL es un lenguaje de programación que se utiliza para el desarrollo de aplicaciones web. Es fundamental para el desarrollo de aplicaciones web escalables y eficientes.",
+      imageUrl: "https://i.scdn.co/image/ab67616d00001e027b349af790501f21719d48d4",
+      album: "back-end",
+      time: "2:30",
+  }
+  ,
+  {
+      id: '10',
+      title: "Laravel",
+      description: "Laravel es un framework de PHP que se utiliza para el desarrollo de aplicaciones web. Es fundamental para el desarrollo de aplicaciones web escalables y eficientes.",
+      imageUrl: "https://i.scdn.co/image/ab67616d00001e02cffeb5001821e9041adcef65",
+      album: "back-end",
+      time: "2:30",
+  }
+  ,
+  {
+      id: '11',
+      title: "React",
+      description: "React es una biblioteca de JavaScript que se utiliza para el desarrollo de aplicaciones web. Es fundamental para el desarrollo de aplicaciones web escalables y eficientes.",
+      imageUrl: "https://i.scdn.co/image/ab67616d00001e0221acc4c231327248498a35da",
+      album: "back-end",
+      time: "2:30",
+  }
+  ,
+  
+  ];
