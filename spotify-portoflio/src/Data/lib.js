50090056da823c53acdfb771e9fb6b6a104f00a8
AlexVennegas <erick.alexis.venegas@gmail.com>
2024-07-07 19:46:53 +0200

    chore: Update SVG icons and styles for consistent design across the application

--- spotify-portoflio/src/Data/lib.js
+++ spotify-portoflio/src/Data/lib.js
@@ -42,7 +42,7 @@ export const Skills = [
   {
       id: '1',
       title: "HTML",
-      description: "lenguaje de marcado que se utiliza para el desarrollo de páginas web",
+      description: "lenguaje de marcas ",
   
       imageUrl: "https://i.scdn.co/image/ab67616d00001e02f0cb4035ee036eb4e42169e5",
       album: "front-end",
@@ -52,7 +52,7 @@ export const Skills = [
   {
       id: '2',
       title: "CSS",
-      description: "lenguaje de estilos que se utiliza para el desarrollo de páginas web.",
+      description: "lenguaje de estilos",
       imageUrl: "https://i.scdn.co/image/ab67616d00001e0235692baca45542527d742d0d",
       album: "front-end",
       time: "2:30",
@@ -61,7 +61,7 @@ export const Skills = [
   {
       id: '3',
       title: "JavaScript",
-      description: "lenguaje de programación que se utiliza para el desarrollo de aplicaciones web",
+      description: "lenguaje de programación web",
       imageUrl: "https://i.scdn.co/image/ab67616d00001e0276e31f565780a5b45c0bbbd8",
       album: "back-end",
       time: "2:30",
@@ -70,7 +70,7 @@ export const Skills = [
   {
       id: '4',
       title: "Python",
-      description: "lenguaje de programación que se utiliza para el desarrollo de aplicaciones web",
+      description: "lenguaje de programación para IA",
       imageUrl: "https://i2o.scdn.co/image/ab67706c0000cfa320f01f28b8e9f8df357957df",
       album: "back-end",
       time: "2:30",
@@ -79,7 +79,7 @@ export const Skills = [
   {
       id: '5',
       title: "C",
-      description: "lenguaje de programación que se utiliza para el desarrollo de aplicaciones web",
+      description: "lenguaje de bajo nivel estructurado",
       imageUrl: "https://i.scdn.co/image/ab67616d00001e02a9249ebb15ca7a5b75f16a90",
       album: "back-end",
       time: "2:30",
@@ -96,7 +96,7 @@ export const Skills = [
   {
       id: '7',
       title: "php",
-      description: "lenguaje de programación que se utiliza para el desarrollo de aplicaciones web",
+      description: "lenguaje de programación web",
       imageUrl: "https://i.scdn.co/image/ab67616d00001e02d699829cd4c29b1b669cb201",
       album: "back-end",
       time: "2:30",
@@ -105,7 +105,7 @@ export const Skills = [
   {
       id: '8',
       title: "Github",
-      description: "Github es una plataforma de desarrollo colaborativo de software para alojar proyectos utilizando el sistema de control de versiones Git. Se utiliza para el desarrollo de aplicaciones web. Es fundamental para el desarrollo de aplicaciones web escalables y eficientes.",
+      description: "plataforma de desarrollo colaborativo",
       imageUrl: "https://image-cdn-ak.spotifycdn.com/image/ab67706c0000da846c3048465107aa5f0aa17393",
       album: "back-end",
       time: "2:30",
@@ -114,7 +114,7 @@ export const Skills = [
   {
       id: '9',
       title: "SQL",
-      description: "SQL es un lenguaje de programación que se utiliza para el desarrollo de aplicaciones web. Es fundamental para el desarrollo de aplicaciones web escalables y eficientes.",
+      description: "lenguaje de consulta estructurada",
       imageUrl: "https://i.scdn.co/image/ab67616d00001e027b349af790501f21719d48d4",
       album: "back-end",
       time: "2:30",
@@ -123,7 +123,7 @@ export const Skills = [
   {
       id: '10',
       title: "Laravel",
-      description: "Laravel es un framework de PHP que se utiliza para el desarrollo de aplicaciones web. Es fundamental para el desarrollo de aplicaciones web escalables y eficientes.",
+      description: "framework de PHP",
       imageUrl: "https://i.scdn.co/image/ab67616d00001e02cffeb5001821e9041adcef65",
       album: "back-end",
       time: "2:30",
@@ -132,7 +132,7 @@ export const Skills = [
   {
       id: '11',
       title: "React",
-      description: "React es una biblioteca de JavaScript que se utiliza para el desarrollo de aplicaciones web. Es fundamental para el desarrollo de aplicaciones web escalables y eficientes.",
+      description: "librería de JavaScript",
       imageUrl: "https://i.scdn.co/image/ab67616d00001e0221acc4c231327248498a35da",
       album: "back-end",
       time: "2:30",
@@ -140,3 +140,156 @@ export const Skills = [
   ,
   
   ];
+
+
+  export const Projects = [
+    {
+        id: '1',
+        title: "AfrooBook",
+        description: "Foro de discusión",
+        imageUrl: "./proyecto1.png",
+        album: "html, css, js, php, sql",
+        time: "2:30",
+    }
+    ,
+    {
+        id: '2',
+        title: "GeeckyGram",
+        description: "Red social para geeks",
+        imageUrl: "./Proyecto5.png",
+        album: "html, css, js, php, sql",
+        time: "2:30",
+    }
+    ,
+    {
+        id: '3',
+        title: "Detector de Mensajes de Odio",
+        description: "Modelo de IA para detectar mensajes de odio",
+        imageUrl: "https://i2o.scdn.co/image/ab67706c0000cfa35f130c3668ccc18e89a720c8",
+        album: "python, machine learning",
+        time: "2:30",
+    }
+    ,
+    {
+        id: '4',
+        title: "F5 Airlines",
+        description: "Prediccion de satisfaccion de clientes",
+        imageUrl: "https://i.scdn.co/image/ab67616d00001e02016e0c0dd9748553cf498983",
+        album: "python, machine learning",
+        time: "2:30",
+    
+    },
+    {
+        id: '5',
+        title: "Danza Fénix",
+        description: "API para la gestión de una escuela de danza",
+        imageUrl: "https://i.scdn.co/image/ab67616d00001e029e0d6948f686dc64958dfe15",
+        album: "python, flask, sql",
+        time: "2:30",
+    },
+    {
+        id: '6',
+        title: "Reconocimiento Facial",
+        description: "Usando CV2 para acceso de seguridad",
+        imageUrl: "https://i.scdn.co/image/ab67616d00001e02d85ba0e3102f838cbbf58409",
+        album: "CV2, python, tkinter",
+        time: "2:30",
+    }
+    ,
+    {
+        id: '7',
+        title: "42Cursus",
+        description: "Programacion en c",
+        imageUrl: "https://i.scdn.co/image/ab67616d00001e02fe23dd528684203b1b2eb754",
+        album: "c, linux, git, bash",
+        time: "2:30",
+    }
+    ,
+    {
+        id: '8',
+        title: "Nasa Space APP",
+        description: "Consumo de API de la NASA",
+        imageUrl: "./Nasa.jpg",
+        album: "analisis de datos, python, flask, sql",
+        time: "2:30",
+    }
+    ,
+    {
+        id: '9',
+        title: "VinoPremier",
+        description: "Programa IA recomendador de vinos",
+        imageUrl: "https://i.scdn.co/image/ab67616d00001e025f12c19b6358841472240173",
+        album: "python, machine learning, sql",
+        time: "2:30",
+    }
+    ,
+    {
+        id: '10',
+        title: "Proceso ETL",
+        description: "Extraccion, transformacion y carga de datos",
+        imageUrl: "https://i.scdn.co/image/ab67616d00001e02f0f7d8fcb934e3407a6ebfac",
+        album: "apache kafka, spark, python, sql",
+        time: "2:30",
+    }
+    ,
+    {
+        id: '11',
+        title: "Oposicione Madrid APP",
+        description: "App para oposiciones en Madrid",
+        imageUrl: "https://i.scdn.co/image/ab67616d00001e029b6e2d17adea514703b623cc",
+        album: "html, css, js, php, sql",
+        time: "2:30",
+    }
+    ,
+    {
+        id: '12',
+        title: "Majica",
+        description: "Museo de arte en linea",
+        imageUrl: "https://i.scdn.co/image/ab67616d00001e028006c6f9d21c761231926abf",
+        album: "html, css, js, php, sql",
+        time: "2:30",
+    }
+    
+    ];
+
+    
+  export const Education = [
+    {
+        id: '1',
+        title: "Confección y publicación de páginas web",
+        description: "Certificado de profesionalidad N.2",
+        imageUrl: "https://www.mostolesdesarrollo.es/images/logo-mostoles-desarrollo.png",
+        album: "html, css, js, WordPress",
+        time: "500hrs",
+    }
+    ,
+    {
+        id: '2',
+        title: "Bootcamp Microsoft IA",
+        description: "Ingeniero de Datos, Analista de Datos, Desarrollador de IA",
+        imageUrl: "https://media.licdn.com/dms/image/C4E0BAQHJ8S2GXbJkAg/company-logo_100_100/0/1656928320986/factoraf5_logo?e=1728518400&v=beta&t=2IPchxSDreuVFPgLrt2gyK2abvoywl0nLMv3W5qughQ",
+        album: "python, jupyter-Ntbook, Dockers, Azure",
+        time: "1250hrs",
+    }
+    ,
+    {
+        id: '3',
+        title: "42 Madrid - Fundación Telefónica",
+        description: "Programación en C",
+        imageUrl: "https://i.scdn.co/image/ab67616d00001e02fe23dd528684203b1b2eb754",
+        album: "python, machine learning",
+        time: "2000hrs", 
+    }
+    ,
+    {
+        id: '4',
+        title: "Bootcamp Full Stack Developer",
+        description: "Desarrollo de aplicaciones web y móviles",
+        imageUrl: "https://larueca.info/wp-content/uploads/2021/08/circuloamarillofondo.png",
+        album: "js, php, Laravel, React, mysql, ",
+        time: "1000hrs",
+    
+    },
+    
+    
+    ];
