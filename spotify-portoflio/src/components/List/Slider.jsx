1a7dddbfe4017b722c45665a697877b661cf30a6
AlexVennegas <erick.alexis.venegas@gmail.com>
2024-07-09 00:03:28 +0200

    refactor: Improve transitions and layout in ArticleModule and [id] page

--- spotify-portoflio/src/components/List/Slider.jsx
+++ spotify-portoflio/src/components/List/Slider.jsx
@@ -1,28 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import './Slider.css';
 
-const images = [
-    "../App/app1.png",
-    "../App/app2.png",
-    "../App/app3.png",
-    "../App/app4.png",
-    "../App/app5.png",
-    "../App/app6.png",
-    "../App/app7.png",
-    "../App/app8.png",
-    "../App/app9.png",
-    "../App/app10.png",
-  
-    "../App/app13.png",
-    // Agrega más rutas de imágenes según sea necesario
-];
+const imageSets = {
+    1: [
+        "../App/presentation1.jpg",
+        "../App/presentation2.jpg",
+        "../App/presentation3.jpg",
+        "../App/presentation4.jpg",
+        "../App/presentation5.jpg",
+        "../App/presentation6.jpg",
+        "../App/presentation7.jpg",
+        
+    ],
+    2: [
+        "../App/app6.png",
+        "../App/app7.png",
+        "../App/app8.png",
+        "../App/app9.png",
+        "../App/app10.png",
+        "../App/app13.png",
+    ],
+};
 
-const Slider = () => {
+const Slider = ({ id }) => {
+    const images = imageSets[id] || [];
     const [slideIndex, setSlideIndex] = useState(0);
     const totalSlides = images.length;
 
     useEffect(() => {
-        const intervalId = setInterval(nextSlide, 5000); // Cambiar cada 10 segundos (10000 milisegundos)
+        const intervalId = setInterval(nextSlide, 5000); // Cambiar cada 5 segundos (5000 milisegundos)
         return () => clearInterval(intervalId);
     }, [slideIndex]);
 
