116d85d2f00dec3138401766a1f321c43cfb3624
AlexVennegas <erick.alexis.venegas@gmail.com>
2024-07-17 22:48:43 +0200

    refactor: Update Astro configuration to include React and Vercel integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [react()],
-  adapter: vercel(),
+  integrations: [react(), vercel()],
+  output: 'server'
 });
