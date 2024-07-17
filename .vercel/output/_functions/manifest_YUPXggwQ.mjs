86cecdb2da859f2e7f633195ffd801862bc8c413
AlexVennegas <erick.alexis.venegas@gmail.com>
2024-07-17 22:55:20 +0200

    refactor: Update TypeScript to version 5.5.3

---
+++ .vercel/output/_functions/manifest_YUPXggwQ.mjs
@@ -0,0 +1,6 @@
+import 'cookie';
+export { m as manifest } from './chunks/_@astrojs-manifest_Cn1Zh01w.mjs';
+import 'kleur/colors';
+import 'html-escaper';
+import 'clsx';
+import './chunks/astro/server_CC0dz80I.mjs';
